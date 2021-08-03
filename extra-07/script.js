5ac101033f56563c0cd8a7c090ead9855d9d8abe
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-08-02 21:22:11 -0300

    Tá valendo a promoção?

---
+++ extra-07/script.js
@@ -0,0 +1,18 @@
+const getYear = require('date-fns/getYear');
+const getDayOfYear = require('date-fns/getDayOfYear');
+
+function promocao(dataInicio, dataCliente) {
+    let taValendo = false;
+
+    if (getYear(dataInicio) === getYear(dataCliente)) {
+        if (getDayOfYear(dataCliente) >= getDayOfYear(dataInicio) && getDayOfYear(dataCliente) <= getDayOfYear(dataInicio) + 30) {
+            taValendo = true;
+        }
+    }
+    return taValendo;
+}
+
+console.log(promocao(new Date(2021, 0, 1), new Date(2021, 0, 23)));
+console.log(promocao(new Date(2021, 0, 1), new Date(2021, 1, 23)));
+console.log(promocao(new Date(2021, 0, 1), new Date(2020, 0, 23)));
+console.log(promocao(new Date(2021, 1, 1), new Date(2021, 2, 3)));
